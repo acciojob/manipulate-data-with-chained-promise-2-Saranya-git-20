d4387101f0d672033aae29d52b7a017c81faf29f
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-08-02 21:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+window.addEventListener('DOMContentLoaded', processArray);
+function processArray(){
+	const array = [1,2,3,4]
+	new Promise((resolve) => {
+		setTimeout(()=>{
+			const even_numer = array.filter(num => num%2 ==0)
+			document.getElementById('output').textContent = even_numer.join(',');
+			resolve(array)
+		},1000)
+	}).then(array=>{
+			return new Promise((resolve)=>{
+				setTimeout(()=>{
+					const double_even = array.filter(num => num%2 ==0).map(num => num*2)
+					document.getElementById('output').textContent = double_even.join(',')
+					resolve()
+				},3000)
+			})
+		}).catch(error=>{
+		console.log(error)
+		})
+}
